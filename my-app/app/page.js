e5907404d44634378087dbc45782db8be5bf4e7a
Rafael Assis <giantrafa@gmail.com>
2026-03-17 12:23:48 +0000

    filho

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,6 +2,7 @@ import styles from "./page.module.css";
 import GeradorAleatorio from "@/components/GeradorAleatorio";
 import TeaGathering from "@/components/loopContador";
 import Profile from "@/components/Profile";
+import GeradorTextoAleatorio from "@/components/GeradorTextoAleatorio";
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
       <GeradorAleatorio/>
       <TeaGathering/>
       <Profile/>
+       <hr />
+      <GeradorTextoAleatorio>Gerador Texto 1</GeradorTextoAleatorio>
+      <hr />
+      <GeradorTextoAleatorio>Gerador Texto 2</GeradorTextoAleatorio>
     </div>
   );
 }

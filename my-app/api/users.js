72c4a1cae9e83749ee87555353d07071ee8b5638
Rafael Assis <giantrafa@gmail.com>
2026-05-08 12:47:53 +0000

    login

---
+++ my-app/api/users.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://parseapi.back4app.com",
+  headers: {
+    "X-Parse-Application-Id": "F9PL8Gv7DCyZflxRptJuVBau0Gs5rjNTwemrdoOk",
+    "X-Parse-REST-API-Key": "Besw4IFlIg560POQEfbSyx5uut8rqi4S8WTRTSXx",
+  },
+});
+const headerJson = { "Content-Type": "application/json" };
+const headerSession = { ...headerJson, "X-Parse-Revocable-Session": "1" };
+const userURL = "/users";
+
+// 'user = { "password":"", "username": "","email": "" }'
+export async function userSignUp(user) {
+  const response = await instance.post(userURL, user, {
+    headers: headerSession,
+  });
+  return response.data;
+}
